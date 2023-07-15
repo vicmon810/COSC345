2032901c04a807e534ef003f01dd904ba09a3abc
maokr879 <maokr879@student.otago.ac.nz>
2023-07-15 16:58:11 +1200

    test

---
+++ BuildME/CMakeFiles/hello.cpp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello.cpp.dir/main.cpp.o"
+  "CMakeFiles/hello.cpp.dir/main.cpp.o.d"
+  "hello.cpp"
+  "hello.cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello.cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ BuildME/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/apple/COSC345/COSC345/BuildME
+# Build directory: /Users/apple/COSC345/COSC345/BuildME
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
