5244db48526572ceaac458ace85ef1b41b6bf521
maokr879 <maokr879@student.otago.ac.nz>
2023-07-17 10:13:57 +1200

    Cmake works, only need to change lib location

---
+++ build/CMakeFiles/MovidAndFood.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MovidAndFood.dir/Connection.cpp.o"
+  "CMakeFiles/MovidAndFood.dir/Connection.cpp.o.d"
+  "MovidAndFood"
+  "MovidAndFood.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MovidAndFood.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/MovidAndFood.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for MovidAndFood.
+# This may be replaced when dependencies are built.
