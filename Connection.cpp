cce437d366bcbebdd0a6c1281952c363a1a99f3b
gohan400 <gohan400@student.otago.ac.nz>
2023-08-08 16:47:48 +1200

    Updated connection so it can gather food detail

--- Connection.cpp
+++ Connection.cpp
@@ -9,48 +9,63 @@ namespace cosc345
 {
     Connection::Connection() {}
 
-    /*TESTING*/
-    // cout << "mongocxx version: " << MONGOCXX_VERSION_STRING << endl;
+    /**
+    * Connect to MongoDB and get all the data
+    */
     void Connection::est_conn()
     {
+        //Establish connection
         mongocxx::instance inst{};
         mongocxx::client conn{mongocxx::uri{"mongodb+srv://admin2:1EiG7CUoKjvMn6p9@cluster0.hayoush.mongodb.net/test"}};
 
         bsoncxx::builder::stream::document document{};
         collection_movie = conn["MOVIE_APP"]["Movies"];
-        collection_food = conn["MOVIE_APP"]["Recipe"];
+        collection_food = conn["MOVIE_APP"]["Food"];
         size_movie = 0;
         size_food = 0;
         auto cursor_movie = collection_movie.find({});
         auto cursor_food = collection_food.find({});
         moviesDetail;
+
+
         // count for movie size
         for (auto &&doc : cursor_movie)
         {
             size_movie++;
+            //Instance of struct
             Movies movie;
+            //If statements are needed to check if the column still exists
             if (doc["title"])
                 movie.title = string(doc["title"].get_utf8().value);
             if (doc["genres"])
-                movie.genres = doc["genres"].get_utf8().value.to_string();
+                movie.genres = string(doc["genres"].get_utf8().value);
             if (doc["imdb_id"])
-                movie.imdb_id = doc["imdb_id"].get_utf8().value.to_string();
+                movie.imdb_id = string(doc["imdb_id"].get_utf8().value);
             if (doc["overview"])
-                movie.overview = doc["overview"].get_utf8().value.to_string();
+                movie.overview = string(doc["overview"].get_utf8().value);
             if (doc["release_date"])
-                movie.release_date = doc["release_date"].get_int32().value;
+                movie.release_date = doc["release_date"].get_int32();
             if (doc["runtime"])
-                movie.runtime = doc["runtime"].get_int32().value;
+                movie.runtime = doc["runtime"].get_int32();
             if (doc["rating"])
-
-                movie.rating = doc["rating"].get_double().value;
+                movie.rating = doc["rating"].get_double();
 
             moviesDetail.push_back(movie);
         }
 
         // count for food size
-        for (auto &&doc : cursor_food)
+        for (auto&& doc : cursor_food) {
             size_food++;
+            //Instance of struct
+            Food food;
+            //If statements are needed to check if the column still exists
+            food.title = string(doc["title"].get_utf8().value);
+            food.ingredients = string(doc["ingredients"].get_utf8().value);
+            food.directions = string(doc["directions"].get_utf8().value);
+            food.NER = string(doc["NER"].get_utf8().value);
+
+            foodDetail.push_back(food);
+        }
     }
 
     /*
@@ -69,8 +84,19 @@ namespace cosc345
         return size_food;
     }
 
+    /**
+    * Returns the details of all movies
+    */
     vector<Connection::Movies> Connection::getDetailMovie()
     {
         return moviesDetail;
+    }
+
+    /**
+    * Returns the details of all food
+    */
+    vector<Connection::Food> Connection::getDetailFood()
+    {
+        return foodDetail;
     }
 }
