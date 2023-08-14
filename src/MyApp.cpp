2e56dd997a131ca5a226261cdf8db394ff81bc17
andre2410 <andrewgoh2000@gmail.com>
2023-08-14 12:36:58 +1200

    Fixed html bug

--- src/MyApp.cpp
+++ src/MyApp.cpp
@@ -30,7 +30,7 @@ MyApp::MyApp() {
   ///
   /// Load a page into our overlay's View
   ///
-  overlay_->view()->LoadURL("file:///app.html");
+  overlay_->view()->LoadURL("file:///Front_end///app.html");
 
   ///
   /// Register our MyApp instance as an AppListener so we can handle the
