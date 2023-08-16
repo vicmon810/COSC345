8624e8965f521addfd90798a75c778c11db609c2
maokr879 <maokr879@student.otago.ac.nz>
2023-08-16 21:29:18 +1200

    fix backend can't sent data to frontend bug

--- src/MyApp.cpp
+++ src/MyApp.cpp
@@ -1,20 +1,27 @@
 #include "MyApp.h"
 
-#define WINDOW_WIDTH  600
+#include ".././backend/include/connection.h"
+
+#define WINDOW_WIDTH 600
 #define WINDOW_HEIGHT 400
 
-MyApp::MyApp() {
+MyApp::MyApp()
+{
   ///
   /// Create our main App instance.
   ///
   app_ = App::Create();
-
+  // initila connection function here
+  cosc345::Connection con;
+  con.est_conn();
+  // test:
+  cout << con.getSizeFood() << endl;
   ///
   /// Create a resizable window by passing by OR'ing our window flags with
   /// kWindowFlags_Resizable.
   ///
   window_ = Window::Create(app_->main_monitor(), WINDOW_WIDTH, WINDOW_HEIGHT,
-    false, kWindowFlags_Titled | kWindowFlags_Resizable);
+                           false, kWindowFlags_Titled | kWindowFlags_Resizable);
 
   ///
   /// Create our HTML overlay-- we don't care about its initial size and
@@ -57,14 +64,17 @@ MyApp::MyApp() {
   overlay_->view()->set_view_listener(this);
 }
 
-MyApp::~MyApp() {
+MyApp::~MyApp()
+{
 }
 
-void MyApp::Run() {
+void MyApp::Run()
+{
   app_->Run();
 }
 
-void MyApp::OnUpdate() {
+void MyApp::OnUpdate()
+{
   ///
   /// This is called repeatedly from the application's update loop.
   ///
@@ -72,11 +82,13 @@ void MyApp::OnUpdate() {
   ///
 }
 
-void MyApp::OnClose(ultralight::Window* window) {
+void MyApp::OnClose(ultralight::Window *window)
+{
   app_->Quit();
 }
 
-void MyApp::OnResize(ultralight::Window* window, uint32_t width, uint32_t height) {
+void MyApp::OnResize(ultralight::Window *window, uint32_t width, uint32_t height)
+{
   ///
   /// This is called whenever the window changes size (values in pixels).
   ///
@@ -85,19 +97,21 @@ void MyApp::OnResize(ultralight::Window* window, uint32_t width, uint32_t height
   overlay_->Resize(width, height);
 }
 
-void MyApp::OnFinishLoading(ultralight::View* caller,
+void MyApp::OnFinishLoading(ultralight::View *caller,
                             uint64_t frame_id,
                             bool is_main_frame,
-                            const String& url) {
+                            const String &url)
+{
   ///
   /// This is called when a frame finishes loading on the page.
   ///
 }
 
-void MyApp::OnDOMReady(ultralight::View* caller,
+void MyApp::OnDOMReady(ultralight::View *caller,
                        uint64_t frame_id,
                        bool is_main_frame,
-                       const String& url) {
+                       const String &url)
+{
   ///
   /// This is called when a frame's DOM has finished loading on the page.
   ///
@@ -105,8 +119,9 @@ void MyApp::OnDOMReady(ultralight::View* caller,
   ///
 }
 
-void MyApp::OnChangeCursor(ultralight::View* caller,
-                           Cursor cursor) {
+void MyApp::OnChangeCursor(ultralight::View *caller,
+                           Cursor cursor)
+{
   ///
   /// This is called whenever the page requests to change the cursor.
   ///
@@ -115,8 +130,9 @@ void MyApp::OnChangeCursor(ultralight::View* caller,
   window_->SetCursor(cursor);
 }
 
-void MyApp::OnChangeTitle(ultralight::View* caller,
-                          const String& title) {
+void MyApp::OnChangeTitle(ultralight::View *caller,
+                          const String &title)
+{
   ///
   /// This is called whenever the page requests to change the title.
   ///
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 #include "MyApp.h"
-
-int main() {
+#include ".././backend/include/connection.h"
+int main()
+{
   MyApp app;
   app.Run();
-
+  cosc345::Connection con;
+  con.est_conn();
+  cout << "Here" << endl;
+  cout << con.getSizeFood() << endl;
   return 0;
 }
