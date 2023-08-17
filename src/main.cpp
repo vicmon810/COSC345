ffa82902fad978804ff108dbaeb2903054d5c956
maokr879 <maokr879@student.otago.ac.nz>
2023-08-17 13:25:21 +1200

    display data on Qt

--- src/main.cpp
+++ src/main.cpp
@@ -1,91 +1,86 @@
-/***
- * https://cpp.hotexamples.com/examples/-/QMenu/-/cpp-qmenu-class-examples.html
- */
-#include <iostream>
-#include "../include/Connection.h"
-// #include "../include/GUI.h"
 #include <QApplication>
 #include <QPushButton>
 #include <QMenu>
 #include <QObject>
 #include <QFrame>
 #include <QHBoxLayout>
-#include <QTextFormat>
-#include <QCheckBox>
 #include <QVBoxLayout>
+#include <QCheckBox>
 #include <QLineEdit>
-/**
- * Main file to run everything
- */
-int main(int argc, char **argv) // As GUI thing needs this apparently
+#include <QTableWidget>
+#include <QTableWidgetItem>
+#include <iostream> //test, delete after use
+#include <QDebug>
+#include "../include/Connection.h"
+int main(int argc, char **argv)
 {
-    using namespace std;
-    using namespace cosc345;
-
-    // Initialise QT App
-    // QApplication app(argc, argv);
-
-    // Test connection to database
-    // Connection conn1;
-    // conn1.est_conn();
-    // conn1.getDetailMovie();
-    // cout << "Total Moive: " << conn.getSizeMovie() << endl;
-    // cout << "Total Recipe: " << conn.getSizeFood() << endl;
-    // cout << "detail" << conn.getDetail() << endl;
-    //  GUI button;
-    //  button.slots();
-    // Initialise QT App
     QApplication app(argc, argv);
-    QPushButton button("Click Me");
-    QFrame frame;
 
-    // QLineEdit text(movie);
-    // Init QMenu
     QMenu *menu = new QMenu();
     menu->setTitle(QObject::tr("Movie and Food"));
-    // Add new Options
-    menu->addAction(QObject::tr("I Feel Luck"));
+    menu->addAction(QObject::tr("I Feel Lucky"));
     menu->addAction(QObject::tr("Open"));
     menu->addAction(QObject::tr("Save"));
-    // Sub----Menu
     QMenu *submenu = menu->addMenu(QObject::tr("Settings"));
     submenu->addAction(QObject::tr("Preferences"));
     submenu->addAction(QObject::tr("Language"));
 
-    // frame.setHidden(menu);
-    // Layout for virtual display
-    QHBoxLayout *layout = new QHBoxLayout;
-    // layout for horntail displayy
+    QFrame frame;
+    QPushButton button("Click Me");
     QVBoxLayout *layout_v = new QVBoxLayout;
-    // add menu into frame virtual
-    layout->addWidget(menu);
-    layout->addSpacing(10);
-
-    // Connecting into DB
-    Connection conn;
-    conn.est_conn();
-    // conn.getDetailMovie();
-    // Initial Qt Objects
     QCheckBox *checkBox = new QCheckBox();
     QLineEdit *lineEdit = new QLineEdit();
 
-    // Fill words in CheckBOX
-    checkBox->setText("Movie Size: " + QString::number(conn.getSizeMovie()));
+    checkBox->setText("Movie Size: 10"); // You can replace "10" with the actual movie size
     checkBox->setChecked(true);
 
-    // Fill words in lineEdit with read only
-    lineEdit->setText("Receipt Size: " + QString::number(conn.getSizeFood()));
+    lineEdit->setText("Receipt Size: 20"); // You can replace "20" with the actual receipt size
     lineEdit->setReadOnly(true);
 
-    // alignments
+    QTableWidget *tableWidget = new QTableWidget();
+    QStringList headers = {"Genres", "IMDB ID", "Overview", "Release Date", "Runtime", "Title", "Rating"};
+    tableWidget->setColumnCount(headers.size());
+    tableWidget->setHorizontalHeaderLabels(headers);
+    const cosc345::Connection conn;
+    conn.est_conn();
+    int db_size = conn.getSizeMovie();
+    tableWidget->setRowCount(db_size); // Set row count based on data size
+
+    for (int row = 0; row < db_size; ++row)
+    {
+        const cosc345::Connection::Movies &movie = moviesData[row]; // Get each movie
+        QTableWidgetItem *genresItem = new QTableWidgetItem(QString::fromStdString(movie.genres));
+        QTableWidgetItem *imdbIdItem = new QTableWidgetItem(QString::fromStdString(movie.imdb_id));
+        QTableWidgetItem *overviewItem = new QTableWidgetItem(QString::fromStdString(movie.overview));
+        QTableWidgetItem *releaseDateItem = new QTableWidgetItem(QString::number(movie.release_date));
+        QTableWidgetItem *runtimeItem = new QTableWidgetItem(QString::number(movie.runtime));
+        QTableWidgetItem *titleItem = new QTableWidgetItem(QString::fromStdString(movie.title));
+        QTableWidgetItem *ratingItem = new QTableWidgetItem(QString::number(movie.rating));
+
+        // Add items to the table widget in the corresponding columns
+        tableWidget->setItem(row, 0, genresItem);
+        tableWidget->setItem(row, 1, imdbIdItem);
+        tableWidget->setItem(row, 2, overviewItem);
+        tableWidget->setItem(row, 3, releaseDateItem);
+        tableWidget->setItem(row, 4, runtimeItem);
+        tableWidget->setItem(row, 5, titleItem);
+        tableWidget->setItem(row, 6, ratingItem);
+    }
+
+    // Add the table widget to your layout or window
+    // For example:
+    layout_v->addWidget(tableWidget);
+
+    QVBoxLayout *layout = new QVBoxLayout;
+    layout->addWidget(menu);
+    // layout->addWidget(button);
+    layout->addWidget(tableWidget); // Add the QTableWidget to the layout
     layout->addLayout(layout_v);
     layout_v->addWidget(checkBox);
-    layout_v->addSpacing(100);
     layout_v->addWidget(lineEdit);
 
-    // display frame
     frame.setLayout(layout);
     frame.show();
+
     return app.exec();
-    // return 0;
 }
