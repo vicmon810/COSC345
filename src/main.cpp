b85b47beb37dfc0b14dcc378b227a1f01f6b8a41
maokr879 <maokr879@student.otago.ac.nz>
2023-08-17 15:07:06 +1200

    transfor form Qtable to QList

--- src/main.cpp
+++ src/main.cpp
@@ -11,11 +11,23 @@
 #include <QTableWidgetItem>
 #include <iostream> //test, delete after use
 #include <QDebug>
+#include <QListWidget>
 #include "../include/Connection.h"
 int main(int argc, char **argv)
 {
     QApplication app(argc, argv);
 
+    // initialize QFrame
+    QFrame frame;
+    QFrame *line = new QFrame();
+    line->setFrameShape(QFrame::HLine);
+    line->setFrameShadow(QFrame::Sunken);
+    frame.setFrameStyle(QFrame::Panel | QFrame::Raised);
+    frame.setLineWidth(2);
+    // Set the default size of QFrame
+    // QVBoxLayout *layout = new QVBoxLayout;
+
+    // initialize menu
     QMenu *menu = new QMenu();
     menu->setTitle(QObject::tr("Movie and Food"));
     menu->addAction(QObject::tr("I Feel Lucky"));
@@ -25,61 +37,43 @@ int main(int argc, char **argv)
     submenu->addAction(QObject::tr("Preferences"));
     submenu->addAction(QObject::tr("Language"));
 
-    QFrame frame;
-    QPushButton button("Click Me");
-    QVBoxLayout *layout_v = new QVBoxLayout;
-    QCheckBox *checkBox = new QCheckBox();
-    QLineEdit *lineEdit = new QLineEdit();
+    // Initialize layout
+    QVBoxLayout *mainLayout = new QVBoxLayout;
+    QGridLayout *movieLayout = new QGridLayout;
 
-    checkBox->setText("Movie Size: 10"); // You can replace "10" with the actual movie size
-    checkBox->setChecked(true);
-
-    lineEdit->setText("Receipt Size: 20"); // You can replace "20" with the actual receipt size
-    lineEdit->setReadOnly(true);
-
+    // Query data from back-end
     cosc345::Connection conn;
     conn.est_conn();
     vector<cosc345::Connection::Movies> movies = conn.getDetailMovie();
 
-    QTableWidget *tableWidget = new QTableWidget();
-    QStringList headers = {"Genres", "IMDB ID", "Overview", "Release Date", "Runtime", "Title", "Rating", "Food"};
-    tableWidget->setColumnCount(headers.size());
-    tableWidget->setHorizontalHeaderLabels(headers);
+    // Initialize QList to for display data set
+    QListWidget *listWidget = new QListWidget();
 
-    int db_size = conn.getSizeMovie();
-    tableWidget->setRowCount(db_size);
-
-    for (int row = 0; row < db_size; ++row)
+    for (const cosc345::Connection::Movies &movie : movies)
     {
-        const cosc345::Connection::Movies &movie = movies[row];
-        QTableWidgetItem *genresItem = new QTableWidgetItem(QString::fromStdString(movie.genres));
-        QTableWidgetItem *imdbItem = new QTableWidgetItem(QString::fromStdString(movie.imdb_id));
-        QTableWidgetItem *overviewItem = new QTableWidgetItem(QString::fromStdString(movie.overview));
-        QTableWidgetItem *dateItem = new QTableWidgetItem(QString::number(movie.release_date));
-        QTableWidgetItem *runtimeItem = new QTableWidgetItem(QString::number(movie.runtime));
-        QTableWidgetItem *titleItem = new QTableWidgetItem(QString::fromStdString(movie.title));
-        QTableWidgetItem *ratingItem = new QTableWidgetItem(QString::number(movie.rating));
-        QTableWidgetItem *foodItem = new QTableWidgetItem("Popcorn");
-        tableWidget->setItem(row, 0, genresItem);
-        tableWidget->setItem(row, 1, imdbItem);
-        tableWidget->setItem(row, 2, overviewItem);
-        tableWidget->setItem(row, 3, dateItem);
-        tableWidget->setItem(row, 4, runtimeItem);
-        tableWidget->setItem(row, 5, titleItem);
-        tableWidget->setItem(row, 6, ratingItem);
-        tableWidget->setItem(row, 7, foodItem);
-        // ... (set other items)
+        QString movieDetails = "Title: " + QString::fromStdString(movie.title) + "\n" +
+                               "Genres: " + QString::fromStdString(movie.genres) + "\n" + "IMDB ID: " + QString::fromStdString(movie.imdb_id) + "\n" + "Overview: " + QString::fromStdString(movie.overview) + "\n" + "Release Date: " + QString::number(movie.release_date) + "\n" + "Runtime: " + QString::number(movie.runtime) + "\n" + "Rating: " + QString::number(movie.rating) + "\n" + "Food: Popcorn";
+        // we only display 50 chars
+        const int maxDisplayLen = 50;
+        if (movieDetails.length() > maxDisplayLen)
+        {
+            movieDetails.truncate(maxDisplayLen); // Truncate the string
+            movieDetails += "...";                // Add ellipses to indicate truncation
+        }
+
+        QListWidgetItem *item = new QListWidgetItem(movieDetails);
+        item->setSizeHint(QSize(75, 75)); // Set the size of each card
+        listWidget->addItem(item);
+        // Add a separator line
     }
-
-    QVBoxLayout *layout = new QVBoxLayout;
-    layout->addWidget(menu);
-    // layout->addWidget(button);
-    layout->addWidget(tableWidget); // Add the QTableWidget to the layout
-    layout->addLayout(layout_v);
-    layout_v->addWidget(checkBox);
-    layout_v->addWidget(lineEdit);
-
-    frame.setLayout(layout);
+    // Append menu/Qlist on main layout
+    // mainLayout->addWidget(&frame);
+    mainLayout->addWidget(menu);
+    movieLayout->addWidget(listWidget);
+    mainLayout->addLayout(movieLayout);
+    movieLayout->addWidget(line);
+    // display manLayout
+    frame.setLayout(mainLayout);
     frame.show();
 
     return app.exec();
