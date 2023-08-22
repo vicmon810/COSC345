390fe159974c67ba61d55d1fec6fabfd82710291
maokr879 <maokr879@student.otago.ac.nz>
2023-08-22 21:21:09 +1200

    main page:

--- src/main.h
+++ src/main.h
@@ -27,7 +27,9 @@
 
 /*! \mainpage Movie and Food
  *   \section intro Introduction
- *  Introduction TODO
+ *  Key Features:
+ *1: Movie Recommendation: The app will analyze the user's movie preferences, ratings, and history to suggest new movies that align with their interests. It will consider factors like genre, cast, ratings, and release year.
+ *2: Food Pairing Suggestions: In addition to movie recommendations, the app will suggest suitable food options that complement the user's selected movie. It will provide recipes based on the movie chosen.
  *   \section install_sec installation
  *
  */
