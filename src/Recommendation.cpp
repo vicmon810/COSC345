77bd9725ff105b2b76b3e23c249ca4cf429b3c8b
andre2410 <andrewgoh2000@gmail.com>
2023-09-13 20:15:17 +1200

    Touched Recommendation.cpp/h, progress in implementing recommendation functions, yay!

--- src/Recommendation.cpp
+++ src/Recommendation.cpp
@@ -8,32 +8,45 @@ namespace cosc345
     //Default constructor
     Recommendation::Recommendation() {}
 
-    Recommendation::Movies conditional(vector<string> genres, vector<double> rating, bool random) {
+    void Recommendation::init() {
+        //Create own connection class
+        Connection conn; 
+        conn.est_conn(); 
+
+        vector<Connection::Movies> movies = conn.getDetailMovie(); 
+        vector<Connection::Food> moviesfoodDetail = conn.getDetailFood(); 
+
+        //Transfer Connection::Movies to Recommendation::Movie since they are from
+        //different classes. So I dont have to use Connection:: Connection:: Connection::
+        //for everything I do in recommendation.cpp. Also the recommendation functions
+        //cant really return a Connection:: item in a Recommendation:: function, C++ is ass
+        for (const cosc345::Connection::Movies& mov : movies) 
+        {
+            //Initialise movie
+            Recommendation::Movie movie;
+            //Transfer data into movie
+            movie.genres = mov.genres;
+            movie.poster = mov.poster;
+            movie.imdb_id = mov.imdb_id;
+            movie.overview = mov.overview;
+            movie.release_date = mov.release_date;
+            movie.runtime = mov.runtime;
+            movie.title = mov.title;
+            movie.rating = mov.rating;
+
+            moviesList.push_back(movie);
+        }
+
+        //Do it for food as well
+        //coming soon
+    }
+
+    Recommendation::Movie movieSelect(vector<string> genres, double rating) {
 
         //separate vector of movies
         vector<Connection::Movies> sorted;
 
-        Recommendation::Movies movie; //Return empty struct for now
-
-        //Random select
-        if (random == true) {
-            //Random genre
-            int random = rand() % genres.size();
-            string random_genre = genres[random]; 
-
-            //Random rating
-            random_device rd; 
-            mt19937 gen(rd()); 
-            uniform_real_distribution<double> distribution(0.0, 5.0); 
-            double random_rating = distribution(gen);  
-
-            //Select random movie
-            bool check = false;
-            while (check) {
-                //Select movie based on random genre and rating
-                check = false; // to not start infinite loop for now
-            }
-        }
+        Recommendation::Movie movie; //Return empty struct for now
 
         //Loop through movie struct vector and append genres and ratings related to that
 
@@ -41,15 +54,28 @@ namespace cosc345
 
     }
 
-    /**
-    * Method for the conditional select mode
-    */
-    Recommendation::Movies tournament(vector<string> genres, vector<double> rating, bool random) {   
-        Recommendation::Movies movie; //Return empty struct for now 
+    Recommendation::Movie randomMovieSelect(vector<string> genres, double rating) {
+        //Random genre
+        int random = rand() % genres.size();
+        string random_genre = genres[random];
 
-        return movie;
+        //Random rating
+        random_device rd;
+        mt19937 gen(rd());
+        uniform_real_distribution<double> distribution(0.0, 5.0);
+        double random_rating = distribution(gen);
+
+        //Select random movie
+        bool check = false;
+        while (check) {
+            //Select movie based on random genre and rating
+            check = false; // to not start infinite loop for now
+        }
+
+        Recommendation::Movie movie; //Return empty struct for now 
+
+        return movie; //Return empty struct for now 
     }
-
 
     /**
     * Method to return random food
