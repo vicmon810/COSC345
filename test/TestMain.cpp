a4d82f3281ec2dda734775be27358fe2319db460
maokr879 <maokr879@student.otago.ac.nz>
2023-09-19 11:28:12 +1200

    test case update

--- test/TestMain.cpp
+++ test/TestMain.cpp
@@ -14,20 +14,20 @@ int main(int argc, char *argv[])
     test1.testGetFoodSize();
     test1.testRecommendation();
 
-    testFrontend test2;
-    QApplication app(argc, argv);
+    // testFrontend test2;
+    // QApplication app(argc, argv);
 
-    // Start a QTimer to exit the application after 2 seconds (2000 ms)
-    QTimer exitTimer;
-    exitTimer.setSingleShot(true);
-    QObject::connect(&exitTimer, &QTimer::timeout, [&]()
-                     {
-                         std::cout << "\nAll tests passed!\n";
-                         app.quit(); // Quit the application after tests are done
-                     });
-    exitTimer.start(2000); // 2000 ms = 2 seconds
+    // // Start a QTimer to exit the application after 2 seconds (2000 ms)
+    // QTimer exitTimer;
+    // exitTimer.setSingleShot(true);
+    // QObject::connect(&exitTimer, &QTimer::timeout, [&]()
+    //                  {
+    //                      std::cout << "\nAll tests passed!\n";
+    //                      app.quit(); // Quit the application after tests are done
+    //                  });
+    // exitTimer.start(2000); // 2000 ms = 2 seconds
 
-    test2.testClickHandler();
+    // test2.testClickHandler();
 
-    return app.exec(); // Start the Qt event loop
+    // return app.exec(); // Start the Qt event loop
 }
